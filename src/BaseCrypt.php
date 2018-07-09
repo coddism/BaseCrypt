cd72a9bec9162007ace12943d9484ea63f74e359
iSergium <isergium@gmail.com>
2018-07-09 23:44:08 +0500

    1.0

--- src/BaseCrypt.php
+++ src/BaseCrypt.php
@@ -4,33 +4,6 @@ namespace Coddism\BaseCrypt;
 
 class BaseCrypt
 {
-    /*static protected $sets = [
-        '0-9' => [
-            'codes' => [
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-            ],
-            'bits' => 4
-        ],
-        'A-Z' => [
-            'codes' => [
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-                'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-                'U', 'V', 'W', 'X', 'Y', 'Z', ' '
-            ],
-            'bits' => 5
-        ],
-        'i-json' => [
-            'codes' => [
-                ':', ',', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1',
-                '2', '3', '4', '5', '6', '7', '8', '9',
-            ],
-            'bits' => 6
-        ],
-
-    ];*/
-
     static protected $importCodes;
     static protected $inBits;
 
